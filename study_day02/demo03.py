befc6e06ddcac6d1a2344c86148a5fe23bbe8e2c
luctoy <luctoy@gmail.com>
2019-06-27 11:16:07 +0800

    测试

---
+++ study_day02/demo03.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from PIL import Image
+
+im = Image.open('../data/phone.jpg')
+im = np.array(im)
+print(im)
+im = [255, 255, 255]-im
+print(im)
+
+Image.fromarray(im.astype(np.uint8)).save('../data/phone2.jpg')
